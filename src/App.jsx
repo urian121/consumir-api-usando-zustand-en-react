8a16b459e4e40149cba828fae14d043d1da00d14
URIAN VIERA <urian1213viera@gmail.com>
2024-05-18 13:16:15 -0500

    iniciando git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Api } from "./zustand/stores/Api";
+
+function App() {
+  return (
+    <>
+      <Api />
+    </>
+  );
+}
+
+export default App;
---
+++ src/zustand/stores/Api.jsx
@@ -0,0 +1,3 @@
+export const Api = () => {
+  return <div>Api</div>;
+};
